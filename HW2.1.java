43856d52c5d9ff3ff40831175353e61539dce127
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-05-30 18:54:11 +0300

    Update HW2.1.java

--- HW2.1.java
+++ HW2.1.java
@@ -1,1 +1,20 @@
+package com.company;
+
+public class BooleanExpression {
+
+    public static void main(String[] args) {
+        BooleanExpression start = new BooleanExpression();
+        boolean result = start.booleanExpression(true, true, false, false);
+        System.out.println(result);
+    }
+
+    public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
+        if ((a & b & !c & !d) || (a & !b & c & !d) || (a & !b & !c & d)
+                || (!a & b & c & !d) || (!a & b & !c & d) || (!a & !b & c & d)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
 

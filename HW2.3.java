c54515a198eda37aa9fde05d0af82a6343d2beed
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-05-30 18:54:43 +0300

    Update HW2.3.java

--- HW2.3.java
+++ HW2.3.java
@@ -1,1 +1,28 @@
+package com.company;
 
+import java.util.Scanner;
+
+public class MathExpression {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        MathExpression start = new MathExpression();
+        System.out.print("Enter the first number: ");
+        float firstNumber = input.nextFloat();
+        System.out.print("Enter the first number: ");
+        float secondNumber = input.nextFloat();
+        System.out.print("Enter the first number: ");
+        float result = input.nextFloat();
+        System.out.print("Result is: ");
+        System.out.print(start.checkMathExpression(firstNumber, secondNumber, result));
+    }
+
+    public boolean check (float firstNumber, float secondNumber, float result) {
+        if (Math.abs((firstNumber + secondNumber) - result) <= 0.0001) {
+            return true;
+        }
+         else {
+            return false;
+        }
+    }
+}
